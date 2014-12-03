1cb6a1bd3d6b81daa224775e852a0c8b5cee4148
rommen <rommen@inbox.lv>
2014-12-03 17:14:42 +0100

    refresh

--- shoe-portal/src/Octagon/ShoePortal/AdminBundle/OctagonShoePortalAdminBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Octagon\ShoePortal\AdminBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class OctagonShoePortalAdminBundle extends Bundle
-{
-}
--- shoe-portal/src/Octagon/ShoePortal/CustomerBundle/OctagonShoePortalCustomerBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Octagon\ShoePortal\CustomerBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class OctagonShoePortalCustomerBundle extends Bundle
-{
-}
