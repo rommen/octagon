7696073762c979b8b3f9ac5f918a4b865abd8b17
rommen <rommen@inbox.lv>
2014-12-12 13:51:03 +0100

    Added shoes to home page. Fixed shoe tile image alignment.

--- shoe-portal/src/Octagon/ShoePortal/CustomerBundle/Controller/HomeController.php
+++ shoe-portal/src/Octagon/ShoePortal/CustomerBundle/Controller/HomeController.php
@@ -4,15 +4,18 @@ namespace Octagon\ShoePortal\CustomerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class HomeController extends Controller
-{
-    public function homeAction()
-    {
-        //Doctrine test
-        
-//        $shoes = $this->getDoctrine()
-//               ->getRepository('CustomerBundle:Shoe')->findAll();
-        $shoes = array();
+class HomeController extends Controller {
+
+    public function homeAction() {
+        //select user shoes
+        $em = $this->getDoctrine()->getManager();
+        $qb = $em->createQueryBuilder()
+                ->select('s')
+                ->from('CustomerBundle:Shoe', 's')
+                ->orderBy('s.idShoe', 'DESC');
+
+        $shoes = $qb->getQuery()->getResult();
         return $this->render('CustomerBundle:Home:home.html.twig', array('shoes' => $shoes));
     }
+
 }
