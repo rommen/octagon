c6f5f7ffd158a354c352fdf6d3064420bbdf31f7
rommen <rommen@inbox.lv>
2015-01-04 14:01:37 +0200

    Implemented admin portal Rating CRUD.

--- shoe-portal/src/Octagon/ShoePortal/AdminBundle/Form/RatingType.php
+++ shoe-portal/src/Octagon/ShoePortal/AdminBundle/Form/RatingType.php
@@ -6,27 +6,25 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class RatingType extends AbstractType
-{
+class RatingType extends AbstractType {
+
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('value')
-            ->add('idOwner')
-            ->add('idSeller')
-            ->add('idShoe')
+                ->add('value')
+//            ->add('idOwner')
+                ->add('idSeller', null, array('label' => 'Seller'))
+                ->add('idShoe', null, array('label' => 'Shoe'))
         ;
     }
-    
+
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'Octagon\ShoePortal\CustomerBundle\Entity\Rating'
         ));
@@ -35,8 +33,8 @@ class RatingType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'octagon_shoeportal_customerbundle_rating';
     }
+
 }
