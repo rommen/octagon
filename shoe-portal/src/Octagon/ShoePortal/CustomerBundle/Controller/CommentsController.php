556c13735f2d9d178de342756bf9f212f922dea5
jayakrishna <raavijayakrishna@yahoo.com>
2015-01-05 16:25:09 +0100

    Comments/comments_add.html.twig Comments/list.html.twig
    fixed

--- shoe-portal/src/Octagon/ShoePortal/CustomerBundle/Controller/CommentsController.php
+++ shoe-portal/src/Octagon/ShoePortal/CustomerBundle/Controller/CommentsController.php
@@ -4,6 +4,8 @@ namespace Octagon\ShoePortal\CustomerBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContextInterface;
+use Octagon\ShoePortal\CustomerBundle\Entity\Comments;
+
 /**
  * Description of CommentsController
  *
@@ -17,27 +19,47 @@ class CommentsController extends SecureController {
      * --sellerId - Hash of the seller id
      * --shoeId - Hash of the shoe id
      */
-    public function addAction(Request $request){
-        
+    public function addAction(Request $request) {
+        $this->checkIfUserLoggedIn();
+
+        //Shoe
+        $comments = new Comments();
+        $comments->setIdOwner($this->getUser());
+
+        $form = $this->createCommentsForm($comments);
+        $form->setAction($this->generateUrl('_comments_add'));
+        $form->add('submit', 'submit', array('label' => 'Add'));
+        $form = $form->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $comments->setDate(new \DateTime());
+
+            $em->persist($comments);
+            $em->flush();
+
+            return $this->redirect('/comments/list');
+        }
+        return $this->render('CustomerBundle:Comments:comments_add.html.twig', array('form' => $form->createView()));
     }
-    
-    /**
-     * List comments of the shoe or of the seller
-     * @param Request $request
-     * --sellerId - Hash of the seller id
-     * --shoeId - Hash of the shoe id
-     */
-    public function listAction(Request $request){
-        
+
+    private function createCommentsForm(Comments $comments) {
+        return $this->createFormBuilder($comments)
+                        ->add('idComments', 'hidden')
+                        ->add('text')
+                        ->add('date')
+                        ->setMethod('POST');
     }
-    
+
     /**
      * Report one particular comment by chaning Comments.reported field to true.
      * Perform AJAX call for this action (example: <button onclick="$.ajax({url: '{{path('_comments_report')}}?id={{m.idMailboxHash}}'})" .....>...</button>
      * @param Request $request
      * --id - hash of the comment id
      */
-    public function reportAction(Request $request){
+    public function reportAction(Request $request) {
         
     }
+
 }
