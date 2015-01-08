3f318e4f9579543822cc327d974a936f6cce975a
jayakrishna <raavijayakrishna@yahoo.com>
2015-01-08 19:05:42 +0100

    ratings

--- shoe-portal/src/Octagon/ShoePortal/CustomerBundle/Controller/RatingController.php
+++ shoe-portal/src/Octagon/ShoePortal/CustomerBundle/Controller/RatingController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Security\Core\SecurityContextInterface;
 use Octagon\ShoePortal\CustomerBundle\Entity\Rating;
 use Octagon\ShoePortal\CustomerBundle\Entity\User;
 use Octagon\ShoePortal\CustomerBundle\Entity\Shoe;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Description of CommentsController
@@ -23,18 +24,43 @@ class RatingController extends SecureController {
      */
     public function addAction(Request $request) {
         $this->checkIfUserLoggedIn();
-        
+        $rating = new Rating();
+        $em = $this->getDoctrine()->getEntityManager();
+        $id = $request->get('sellerId');
+        if ($id == null) {
+            $id = base64_decode($request->get('shoeId'));
+            
+            $qb = $em->createQueryBuilder()
+                ->select('s')
+                ->from('CustomerBundle:Shoe', 's')
+                ->where('s.idShoe = :id')
+                ->setParameter('id', $id);
+            $idShoe = $qb->getQuery()->getOneOrNullResult();
+            $rating->setIdShoe($idShoe);
+        } else {
+            $id = base64_decode($id);
+            $qb = $em->createQueryBuilder()
+                ->select('s')
+                ->from('CustomerBundle:User', 's')
+                ->where('s.idUser = :id')
+                ->setParameter('id', $id);
+            $idSeller = $qb->getQuery()->getOneOrNullResult();
+            $rating->setIdSeller($idSeller);
+        }
+       
+        $rating->setValue($request->get('value'));
+        $rating->setIdOwner($this->getUser());
         /*Request
             value = {1-5}
             sellerId or shoeId - which is not null is right one
          */
-
-        $rating = new Rating();
+       
         //set data from request
         
-        $em = $this->getDoctrine()->getEntityManager();
+        
         $em->persist($rating);
         $em->flush();
+        return new Response('Rated successfully');
     }
 
     /**
